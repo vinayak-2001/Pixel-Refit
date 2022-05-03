b1a24ee4b15dd0942e829284b695d3a50493ba44
vinayak-2001 <72159748+vinayak-2001@users.noreply.github.com>
2022-05-03 15:35:07 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,29 +1,30 @@
-const staticDevCoffee = "pixel-refit-v1"
+const staticDevCoffee = "pixel-refit-v1";
 const assets = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/pexels-alexas-fotos-2255441.jpg",
-    "/manifest.json",
-    "/images/icons/icon-192x192.png",
-    "/images/icons/icon-256x256.png",
-    "/images/icons/icon-384x384.png",
-    "/images/icons/icon-512x512.png",
-    "https://fonts.googleapis.com/css2?family=Staatliches&display=swap",
-]
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(staticPixelRefit).then(cache => {
-            cache.addAll(assets)
-        })
-    )
-})
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+  "/site.webmanifest",
+  "/images/icons/android-chrome-192x192.png",
+  "/images/icons/android-chrome-512x512.png",
+  "/images/icons/apple-touch-icon.png",
+  "/images/icons/favicon-16x16.png",
+  "/images/icons/favicon-32x32.png",
+  "https://fonts.googleapis.com/css2?family=Fredoka+One&family=Teko:wght@300&display=swap",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticPixelRefit).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
